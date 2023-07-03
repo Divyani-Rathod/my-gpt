48b87403a48176c833f8a40248cb63fc3fee522b
DivyaniRathod <rathoddivyani4@gmail.com>
2023-07-03 12:16:14 +0530

    completed summarizer part

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import './App.css' ;
 import Hero from './components/Hero' ;
 import Demo from './components/Demo' ;
-import Footer from './components/Footer';
+
 // import '@fortawesome/fontawesome-free/css/all.min.css';
 
 const App = () => {
